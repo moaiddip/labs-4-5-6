ca11c029916f8a8ebabd018174858fd05f0c6dc3
moaiddip@gmail.com <moaiddip@gmail.com@6d85293d-7676-cd1f-4a9a-7e9f4bd41ab9>
2014-02-10 13:40:59 +0000

    fresh start

--- Labs456/src/labs4/pkg5/pkg6/Labs456.java
+++ Labs456/src/labs4/pkg5/pkg6/Labs456.java
@@ -1,5 +1,4 @@
 /*
-Thanos was here!
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
