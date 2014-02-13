edf59a1888361399bb2ba75c715a15f47e5d74a3
swsassias@gmail.com <swsassias@gmail.com@6d85293d-7676-cd1f-4a9a-7e9f4bd41ab9>
2014-02-13 15:02:13 +0000

    Added login.

--- Labs456/src/labs4/pkg5/pkg6/Labs456.java
+++ Labs456/src/labs4/pkg5/pkg6/Labs456.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package labs4.pkg5.pkg6;
 
 import java.sql.Connection;
@@ -19,22 +18,27 @@ import java.util.logging.Logger;
  * @author moaiddip
  */
 public class Labs456 {
-    public void login(javax.swing.JTextField username, javax.swing.JPasswordField password) throws Exception{
+
+    public void login(javax.swing.JTextField username, javax.swing.JPasswordField password)throws Exception {
         System.out.println("JESTEM TU!");
-        String URL = "jdbc:mysql://127.0.0.1:3306/lab-4-5-6_university_database?user=test&password=test";
-            
-            Connection c = DriverManager.getConnection(URL);
-            Statement st = c.createStatement();
-//            ResultSet rs = st.executeQuery("SELECT Name FROM students"
-//                    + "WHERE Login like " +username+ " AND Password like "+password);
-            ResultSet rs = st.executeQuery("SELECT * FROM students");
+        String name = null;
+        String URL = "jdbc:mysql://127.0.0.1:3306/lab-4-5-6_university_database?user=root&password=pwnage12";
+
+        Connection c = DriverManager.getConnection(URL);
+        Statement st = c.createStatement();
+        String user = username.getText();
+        String pass = password.getText();
+        ResultSet rs = st.executeQuery("SELECT Name FROM students WHERE (Login LIKE '"+user+"' AND Password LIKE '"+pass+"')");
+        //ResultSet rs = st.executeQuery("SELECT * FROM students");
        
 
-            String a = rs.getString("Name");
-        System.out.println("Student Name: " +a);
-    /**
-     * @param args the command line arguments
-     */
-    
-}
+        while (rs.next()){
+            name = rs.getString("Name");
+        }
+        System.out.println(name);
+        /**
+         * @param args the command line arguments
+         */
+
+    }
 }
